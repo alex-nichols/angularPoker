142835d30c929ab53e191b198ca058e41ef06982
Alex Nichols <alexnichols@gmail.com>
2018-02-16 20:26:31 -0800

    minor cleanup

--- angularPoker-app/web/src/app/store/reducers.ts
+++
@@ -1,9 +0,0 @@
-import * as DeckReducer from '../deck/reducers'
-import { ActionReducerMap } from '@ngrx/store';
-
-export interface AppState {
-}
-
-export const reducers: ActionReducerMap<AppState> = {
-    cardComponent: DeckReducer.reducer
-}
